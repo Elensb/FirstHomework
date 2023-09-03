d1477be9ac917adba85ab7f483d5614307957fb1
Elensb <elen.sobchuk@gmail.com>
2023-09-03 14:28:30 +0300

    Initial Commit

---
+++ FirstHomework/FirstHomeworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstHomeworkApp.swift
+//  FirstHomework
+//
+//  Created by Elen Sobchuk on 03.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct FirstHomeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
